982469943038809aa8f8ff8750b72c971e92ab39
guySimple <sig_nal@aliyun.com>
2020-01-07 21:09:14 +0800

    feat: init

---
+++ app/src/main/java/com/example/acousticsensing/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.acousticsensing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // UI component
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
